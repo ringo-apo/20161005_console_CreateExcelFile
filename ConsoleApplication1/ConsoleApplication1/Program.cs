a309c9aea504eab97328a8e8189b363f6a9d5a51
akira <ringo1625@gmail.com>
2016-10-05 20:40:12 +0900

    最初のコミット

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+
+            string ExcelBookFileName = @"D:\test";
+            Application ExcelApp = new Application();
+            ExcelApp.Visible = false;
+            Workbook wb = ExcelApp.Workbooks.Add();
+            Worksheet ws1 = wb.Sheets[1];
+            ws1.Select(Type.Missing);
+            ws1.Cells[1, 1] = a;
+            wb.SaveAs(ExcelBookFileName);
+            wb.Close(false);
+            ExcelApp.Quit();
+
+            Console.ReadKey(); //文字を押すと終了
+        }
+    }
+}
